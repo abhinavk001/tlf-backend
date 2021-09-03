72a495d4e2ab2487ae893678fce264cd39498520
abhinavk001 <abhinavk5544@gmail.com>
2021-09-03 12:48:48 +0530

    added stack attribute to profile/staff endpoint

--- app/dbops/tokens.py
+++ app/dbops/tokens.py
@@ -1,8 +1,9 @@
-import os
+"""
+JWT token operation
+"""
 from datetime import datetime, timedelta
-from jose import JWTError, jwt
+from jose import jwt
 from typing import Optional
-from schemas.user import TokenData
 
 SECRET_KEY = "f2089a467fcd8eac98baed2ccdc7e1f1d18c4cd99334f032298dda7106b13dc3"
 ALGORITHM = 'HS256'
@@ -20,17 +21,3 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
-
-def verify_token(token: str, credential_exception):
-    """
-    Verify JWT token
-    """
-    try:
-        decoded_token = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        email: str = decoded_token.get("sub")
-        if email is None:
-            raise credential_exception
-        token_data = TokenData(email=email)
-    except JWTError:
-        raise credential_exception
-    return decoded_token
--- app/routers/user.py
+++ app/routers/user.py
@@ -4,7 +4,6 @@ Routes for managing users.
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
-from sqlalchemy.sql import roles
 from database import models
 from dbops.oauth2 import get_current_user
 from dbops.commons import get_db, hash_password, commit_changes_to_object
@@ -19,13 +18,13 @@ router = APIRouter(
 )
 
 @router.get("/profile", response_model=ShowUser)
-def get_profile(current_user: models.User = Depends(get_current_user), db: Session = Depends(get_db)):
+def get_profile(current_user: models.User=Depends(get_current_user), db: Session = Depends(get_db)):
     """
     Get the profile of the current user.
     """
     if not current_user:
         raise HTTPException(status_code=404, detail="User not found")
-    
+  
     user_info = db.query(models.User).filter(models.User.email == current_user.email).first()
     return user_info
 
@@ -37,7 +36,7 @@ def get_progess(current_user: ShowUser = Depends(get_current_user), db: Session 
     Get the progress of all facilitators.
     """
     all_activity = db.query(
-                            models.Activity, models.User.name, models.User.id).order_by(
+                            models.Activity, models.User.name, models.User.id, models.User.stack).order_by(
                             models.Activity.assign_date.desc()).filter(
                             models.Activity.user.has(is_active=True)).filter(
                             models.Activity.user_id == models.User.id).all()
@@ -56,7 +55,7 @@ def update_user(id: int, request: UpdateUser, current_user: models.User = Depend
 
     if user.id != current_user.id:
         raise HTTPException(status_code=403, detail="You do not have permission to edit this User")
-    
+
     save_updated_user(db, user, request)
     return {
         "detail": "User updated successfully"
@@ -65,15 +64,16 @@ def update_user(id: int, request: UpdateUser, current_user: models.User = Depend
 
 allow_modify_other_users = RoleChecker(["ADMIN", "MODERATOR"])
 @router.patch("/update/staff/{id}", dependencies=[Depends(allow_modify_other_users)])
-def update_user_by_staff(id: int, request: UpdateUserByStaff, current_user: models.User = Depends(get_current_user), 
-                    db: Session = Depends(get_db)):
+def update_user_by_staff(id: int, request: UpdateUserByStaff,
+                        current_user: models.User = Depends(get_current_user),
+                        db: Session = Depends(get_db)):
     """
     Update an existing User by a staff member.
     """
     user = get_user_by_id(id, db)
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
-    
+
     save_updated_user_by_staff(db, user, request)
     return {
         "detail": "User updated successfully"
@@ -82,17 +82,19 @@ def update_user_by_staff(id: int, request: UpdateUserByStaff, current_user: mode
 
 allow_create_user = RoleChecker(["ADMIN"])
 @router.post("/create", response_model=ShowUser, dependencies=[Depends(allow_create_user)])
-def create_user(request:CreatePrivilagedUser, current_user: ShowUser = Depends(get_current_user), db: Session = Depends(get_db)):
+def create_user(request:CreatePrivilagedUser, current_user: ShowUser = Depends(get_current_user),
+                db: Session = Depends(get_db)):
     """
     Create new admins or mods account
     """
     new_user = models.User(name=request.name, email=request.email, 
-                            password=hash_password(request.password), contact=request.contact, role=request.role)
+                            password=hash_password(request.password), contact=request.contact,
+                            role=request.role)
     try:
         commit_changes_to_object(db, new_user)
     except IntegrityError:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"User already exists")
-    
+
     return new_user
 
 
@@ -105,7 +107,7 @@ def deactivate_user(id: int, current_user: models.User = Depends(get_current_use
     user = get_user_by_id(id, db)
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
-    
+
     save_deactivated_user(db, user)
     return {
         "detail": "User deactivated successfully"
@@ -113,12 +115,12 @@ def deactivate_user(id: int, current_user: models.User = Depends(get_current_use
 
 
 allow_delete_activity = RoleChecker(["ADMIN", "MODERATOR"])
-@router.delete("/delete/{id}", dependencies=[Depends(allow_delete_activity)], status_code=status.HTTP_204_NO_CONTENT)
-def delete(id: int, current_user: ShowUser = Depends(get_current_user), db: Session = Depends(get_db)):
+@router.delete("/delete/{id}", dependencies=[Depends(allow_delete_activity)],
+                status_code=status.HTTP_204_NO_CONTENT)
+def delete(id: int, current_user: ShowUser = Depends(get_current_user),
+            db: Session = Depends(get_db)):
     """
     Delete an existing User
     """
     delete_user(db, id)
     return {"detail": "User deleted"}
-
-    
